264bb20a47c597c5c445fa9ce6a40f4fd99165b9
jaylimbachiya43 <jaylimbachiya43@gmail.com>
2024-01-29 18:06:55 +0530

    test

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,16 @@
 class ProductsController < ApplicationController
 def index
-@products = Product.all
+  @products = Product.all
 end
+
 def show
   @product = Product.find(params[:id])
 end
+
 def new
   @product = Product.new
 end
+
 def create 
   @product = Product.new  
    @product.save
